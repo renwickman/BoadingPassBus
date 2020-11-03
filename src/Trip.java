66ab8756b055d8ed65b8d52deee6382aa7ef9c1e
Anthony Renwick <renwickman@gmail.com>
2020-11-03 15:33:01 -0600

    Few more edits.

--- src/Trip.java
+++ src/Trip.java
@@ -152,12 +152,13 @@ public class Trip {
 
         try {
             session.beginTransaction();
-            //Application currentApp = session.get(Application.class, appId);
-            passenger.setOrigin(enterDepart());
-            passenger.setDestination(enterArrive());
-            passenger.setDepartDate(departDate());
-            passenger.setDepartTime(departTime());
-            passenger.setEta(estTimeArrive());
+            Application currentApp = session.get(Application.class, appId);
+            currentApp.setOrigin(enterDepart());
+            currentApp.setDestination(enterArrive());
+            currentApp.setDepartDate(departDate());
+            currentApp.setDepartTime(departTime());
+            currentApp.setEta(estTimeArrive());
+            passenger = currentApp;
             session.save(passenger);
             session.getTransaction().commit();
         } finally {
